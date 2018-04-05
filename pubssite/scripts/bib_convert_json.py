934502e9132e4df699e4c517ab9cb258d163742d
PatHealy <pwh5@pitt.edu>
2018-04-05 01:29:08 -0400

    Functional conversion of json to csv

--- pubssite/scripts/bib_convert_json.py
+++ pubssite/scripts/bib_convert_json.py
@@ -25,16 +25,50 @@ def scrape(sepdir):
     soup = BeautifulSoup(body.decode('iso-8859-1'),'html.parser')
     #For each citation in the bibliography
     for link in soup.find(id='bibliography').find_all('li'):
-        citations.append(link)
+        citations.append(link.text.replace('\n', ' '))
 
+if len(sys.argv) < 3:
+    print("Syntax: python bib_convert_json.py <json file name> <sepdir>")
+    exit()
+        
 filename = sys.argv[1]
 print('Reading ', filename)
-fn = open(filename)
-contents = json.loads(fn.read())
+fn = open(filename, 'rb')
+fn_stuff = fn.read()
+fn.close()
+contents = json.loads(fn_stuff)
 scrape(sys.argv[2])
 
-for entry,cite in contents,citations:
-    print('Authors: ', entry['author'])
-    print('Title: ', entry['title'])
-    print()
+outFileName = '' + sys.argv[2] + '_bib.csv' 
+csv = open(outFileName, 'w')
+csv.write('Family Name, Given Name, Citation, Google Link\n')
 
+for i in range(len(contents)):
+    #print(citations[i])
+    if 'author' in contents[i].keys():
+        for author in contents[i]['author']:
+            out_line = ''
+            if 'family' in author.keys():
+                out_line += author['family'] + ','
+            else:
+                out_line += ','
+            if 'given' in author.keys():
+                out_line += author['given'] + ',\"'
+            else:
+                out_line += ',\"'
+            out_line += citations[i] + '\",'
+            out_line += 'https://www.google.com/search?q='
+            if 'given' in author.keys():
+                out_line += author['given']
+            if 'family' in author.keys():
+                out_line += '%20' + author['family'] 
+            out_line += '\n'
+            csv.write(out_line)
+    else:
+        out_line = ',,\"' + citations[i] + '\",\n'
+        csv.write(out_line)
+    #print('Title: ', contents[i]['title'])
+    #print()
+
+csv.close()
+print('Completed')
